ab80d8c9573a5a13291a7b3ea7a0d0e58becfe14
johnmaster <544954556@qq.com>
2025-11-29 15:17:48 +0800

    add solution for LeetGPU matrix multiply

---
+++ solution_for_leetGPU/matrix_multiply/kernels.cuh
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <chrono>
+#include <random>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+#define CHECK_CUDA(call)                                                       \
+    do {                                                                       \
+        cudaError_t err = call;                                                \
+        if (err != cudaSuccess) {                                              \
+            fprintf(stderr, "CUDA error %d: %s at %s:%d\n", err,                \
+                    cudaGetErrorString(err), __FILE__, __LINE__);              \
+            exit(1);                                                           \
+        }                                                                      \
+    } while(0)
+
+#define CHECK_CUBLAS(call)                                                     \
+    do {                                                                       \
+        cublasStatus_t err = call;                                             \
+        if (err != CUBLAS_STATUS_SUCCESS) {                                    \
+            fprintf(stderr, "cuBLAS error %d at %s:%d\n", err, __FILE__, __LINE__); \
+            exit(1);                                                           \
+        }                                                                      \
+    } while(0)
+
+#define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
+
+struct Kernel {
+    const char* name;
+    void (*func)(float*, float*, float*, int, int, int);  
+};
+
+void benchMark(int M, int N, int K);
