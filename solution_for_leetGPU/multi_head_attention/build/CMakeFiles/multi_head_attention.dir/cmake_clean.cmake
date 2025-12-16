b13c90d19ff69e9ebd1f0d830a9064d300085f3f
johnmaster <544954556@qq.com>
2025-12-16 22:11:31 +0800

    add multi-head attention

---
+++ solution_for_leetGPU/multi_head_attention/build/CMakeFiles/multi_head_attention.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multi_head_attention.dir/kernels.cu.o"
+  "CMakeFiles/multi_head_attention.dir/kernels.cu.o.d"
+  "CMakeFiles/multi_head_attention.dir/main.cu.o"
+  "CMakeFiles/multi_head_attention.dir/main.cu.o.d"
+  "multi_head_attention"
+  "multi_head_attention.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/multi_head_attention.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
