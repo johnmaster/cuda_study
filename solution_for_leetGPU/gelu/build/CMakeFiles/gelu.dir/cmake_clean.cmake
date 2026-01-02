e487c3e7b6425a15e94cadd924789027dd7f19b8
johnmaster <544954556@qq.com>
2026-01-02 11:36:33 +0800

    add gelu

---
+++ solution_for_leetGPU/gelu/build/CMakeFiles/gelu.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gelu.dir/kernels.cu.o"
+  "CMakeFiles/gelu.dir/kernels.cu.o.d"
+  "CMakeFiles/gelu.dir/main.cu.o"
+  "CMakeFiles/gelu.dir/main.cu.o.d"
+  "gelu"
+  "gelu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/gelu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
